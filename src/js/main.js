1bec6c7bf9b5b9b0c220928650a995307910a7d5
Camila <camilaaparicioo@hotmail.com>
2022-01-21 14:46:37 -0500

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,20 @@ var reportsWidget = {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
+            
                 '<article class="reports_item">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
+
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}} ({{file_size}} {{file_type}})</a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
+
                 '</article>' +
             '{{/.}}'
         )
